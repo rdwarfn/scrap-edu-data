47fb76cfe14b12a6200f9cad904d15f343c40013
Ridwan Arifin <46421706+sukitt@users.noreply.github.com>
2020-05-07 03:35:39 +0700

    Delete scrap.py

--- scrap.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://referensi.data.kemdikbud.go.id/index11.php?kode=010101&level=3'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-result = soup.find(id='example')
-
-print(result.prettify())
